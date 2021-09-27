c6e77abc2349a0380b576243b861e15d612a47f0
agungd3v <mailtotuyul1@gmail.com>
2021-09-27 16:34:24 +0700

    update socket add inmsg

--- app/socket.js
+++ app/socket.js
@@ -4,14 +4,17 @@ const socket = new Server({ cors: { origin: "*" } })
 const on = (port) => {
   const message = require("./models/message")
   socket.on("connection", (socket) => {
-    socket.emit("scnn", { status: true, message: "You are connected!" })
-    socket.on("msg", async (msg) => {
-      socket.emit("getmsg", { status: true, message: { type: "typing", message: "Typing..." } })
+    socket.emit("scnn", { status: true, message: { type: "connection", data: "You are connected!" } })
+    socket.on("msg", () => {
+      socket.emit("getmsg", { status: true, message: { type: "typing", data: "Typing..." } })
+    })
+    socket.on("inmsg", async (msg) => {
       try {
         const data = new message({ user: msg.user, message: msg.message })
         await data.save()
-        socket.emit("getmsg", { status: true, message: { type: "result", message: msg.message } })
+        socket.emit("getmsg", { status: true, message: { type: "result", data: msg.message } })
       } catch (error) {
+        socket.emit("getmsg", { status: true, message: { type: "error", data: "Failed send message, Please contact developer" } })
         throw error
       }
     })
